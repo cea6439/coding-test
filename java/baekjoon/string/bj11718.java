7e72f353b8e18437ee9b23ca21612c7b5745b669
cea6439 <126125547+cea6439@users.noreply.github.com>
2025-04-07 22:56:31 +0900

    [백준/11718] 그대로 출력하기(브론즈3)❌

---
+++ java/baekjoon/string/bj11718.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/*
+  - 문제 풀이: 250404(금)
+  - 11718번: 그대로 출력하기 (브론즈3), https://www.acmicpc.net/problem/11718
+  - 실행 시간: 108ms
+*/
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		
+    // EOF까지 처리
+		while ((str = br.readLine()) != null) {
+		    System.out.println(str);
+		}
+	}
+}
